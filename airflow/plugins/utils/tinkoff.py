deae0c8649fd46f4e4449c2008ee6eb6acd54e44
nick <alex97ander97@gmail.com>
2021-12-26 22:09:26 +0300

    create functions in tinkoff.py: 1) get_position_by_ticker 2) create_limit_order_by_figi 3) get_current_balance

--- airflow/plugins/utils/tinkoff.py
+++ airflow/plugins/utils/tinkoff.py
@@ -2,6 +2,7 @@ import tinvest
 import pandas as pd
 from configparser import ConfigParser
 from datetime import datetime, timedelta
+from typing import Optional
 
 
 def _get_api_params_from_config() -> dict:
@@ -58,3 +59,46 @@ def get_data_by_ticker_and_period(
             'volume',
         )
     )
+
+
+def get_position_by_ticker(ticker: str) -> Optional[tinvest.PortfolioPosition]:
+    client = tinvest.SyncClient(**_get_api_params_from_config())
+    positions = client.get_portfolio().payload.positions
+
+    filtered_position = list(filter(lambda x: x.ticker.lower() == ticker.lower(), positions))
+    if len(filtered_position) == 0:
+        return None
+    return filtered_position[0]
+
+
+def create_limit_order_by_figi(
+        figi: str,
+        lots: int,
+        price: float,
+        op_type: str = 'Buy'
+) -> None:
+    if op_type not in ('Buy', 'Sell'):
+        raise ValueError('Operation type must be Buy or Sell with upper-case first letter')
+    client = tinvest.SyncClient(**_get_api_params_from_config())
+    client.post_orders_limit_order(
+        figi=figi,
+        body=tinvest.schemas.LimitOrderRequest(
+            lots=lots,
+            operation=tinvest.schemas.OperationType(value=op_type),
+            price=price
+        )
+    )
+
+
+def get_current_balance(currency_type: str) -> float:
+    client = tinvest.SyncClient(**_get_api_params_from_config())
+    currencies = client.get_portfolio_currencies().payload.currencies
+
+    filtered_currencies = list(filter(lambda x: x.currency.lower() == currency_type.lower(), currencies))
+
+    if len(filtered_currencies) == 0:
+        return 0.0
+    return float(filtered_currencies[0].balance)
+
+
+
